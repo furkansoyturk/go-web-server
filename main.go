db3a5456ef483b1b0bb9c1a96d734b02699467a6
furkansoyturk <furkansoyturk@icloud.com>
2024-05-18 21:45:46 +0300

    health check and new endpoints added...

--- main.go
+++ main.go
@@ -10,11 +10,29 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 	mux.Handle("/logo", http.FileServer(http.Dir("/assets")))
-
+	mux.HandleFunc("/app", app)
+	mux.HandleFunc("/app/assets", assets)
+	mux.HandleFunc("/healthz", healthzHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving on port: %s \n", port)
 	log.Fatal(server.ListenAndServe())
+
+}
+
+func assets(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, req, "./assets/index.html")
+}
+func app(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, req, "index.html")
+}
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	ok := []byte("OK")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(ok)
 }
