a970cc42ac6871cc323b23699dd779b0ca7b11af
furkansoyturk <furkansoyturk@icloud.com>
2024-08-10 15:34:18 +0300

    ..

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"github.com/furkansoyturk/go-web-server/internal/database"
 	"log"
 	"net/http"
-
-	"github.com/furkansoyturk/go-web-server/internal/database"
 )
 
 type apiConfig struct {
