b0d25fb13b1d2351e7a7769b704e0b7a2e6da344
furkansoyturk <furkansoyturk@icloud.com>
2024-09-11 22:07:35 +0300

    author id query param added to get chirps endpoint

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -12,21 +12,46 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
 		return
 	}
+	authorIdParam := r.URL.Query().Get("author_id")
+	authorId, _ := strconv.Atoi(authorIdParam)
 
-	chirps := []Chirp{}
-	for _, dbChirp := range dbChirps {
-		chirps = append(chirps, Chirp{
-			ID:       dbChirp.ID,
-			Body:     dbChirp.Body,
-			AuthorID: dbChirp.AuthorID,
+	queriedChirps := []Chirp{}
+	if 0 != authorId {
+		for _, dbChirp := range dbChirps {
+			if dbChirp.AuthorID == authorId {
+				queriedChirps = append(queriedChirps, Chirp{
+					ID:       dbChirp.ID,
+					Body:     dbChirp.Body,
+					AuthorID: dbChirp.AuthorID,
+				})
+			}
+		}
+		if len(queriedChirps) == 0 {
+			respondWithError(w, http.StatusNotFound, "Author not found")
+		}
+
+		sort.Slice(queriedChirps, func(i, j int) bool {
+			return queriedChirps[i].ID < queriedChirps[j].ID
 		})
+
+		respondWithJSON(w, http.StatusOK, queriedChirps)
+		return
+	} else {
+		chirps := []Chirp{}
+		for _, dbChirp := range dbChirps {
+			chirps = append(chirps, Chirp{
+				ID:       dbChirp.ID,
+				Body:     dbChirp.Body,
+				AuthorID: dbChirp.AuthorID,
+			})
+		}
+
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].ID < chirps[j].ID
+		})
+
+		respondWithJSON(w, http.StatusOK, chirps)
 	}
-
-	sort.Slice(chirps, func(i, j int) bool {
-		return chirps[i].ID < chirps[j].ID
-	})
-
-	respondWithJSON(w, http.StatusOK, chirps)
 }
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
